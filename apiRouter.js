87d0f167a8b4fa57fa88663fb7af9a3a03a4665f
Thomas Berthaud <tomberthaud@gmail.com>
2018-04-10 00:52:30 +0200

    fix get commit hierarchy + new sunburst graph (wip)

--- apiRouter.js
+++ apiRouter.js
@@ -1,133 +1,110 @@
-var path = require('path');
-var Git = require('nodegit');
-var _ = require('underscore');
-var router = require('express').Router();
+var path = require("path");
+var Git = require("nodegit");
+var _ = require("underscore");
+var router = require("express").Router();
 
-router.get('/repos/:owner/:repoName/commits', function (req, res) {
-    getGitRepo(req.params).then(function (repo) {
-        return repo.getHeadCommit().then(function (firstComm) {
-            var history = firstComm.history();
-            var list = [];
+router.get("/repos/:owner/:repoName/commits", function(req, res) {
+  getGitRepo(req.params)
+    .then(function(repo) {
+      return repo.getHeadCommit().then(function(firstComm) {
+        var history = firstComm.history();
+        var list = [];
 
-            history.on('commit', function (commit) {
-                list.push({
-                    "date": commit.date(),
-                    "author": commit.committer().name(),
-                    "message": commit.message(),
-                    "sha": commit.sha()
-                });
-            })
-
-            history.on('end', function () {
-                res.status(200).json(list);
-            })
-
-            history.on('error', function (err) {
-                console.log(err);
-                res.status(500).send();
-            });
-
-            history.start();
-        })
-    }).catch(function (err) {
-        console.log(err);
-        res.status(404).send();
-    })
-})
-
-router.get('/repos/:owner/:repoName/commits/:sha/files', function (req, res) {
-    getGitRepo(req.params).then(function (repo) {
-        return repo.getCommit(req.params.sha).then(function (commit) {
-            return commit.getTree().then(function (tree) {
-                var filesTree = { name: req.params.repoName, children: [] },
-                    promises = [],
-                    walker = tree.walk(false);
-
-                walker.on('entry', function (entry) {
-                    let node,
-                        hook = filesTree.children;
-                    if (entry.isFile()) {
-                        node = { path: entry.path(), name: entry.name(), size: 0 };
-                        if (entry.isBlob()) {
-                            promises.push(entry.getBlob().then(function (blob) {
-                                node.size = blob.content().toString().split("\n").length;
-                            }));
-                        }
-                    } else {
-                        node = { name: entry.name(), children: [] }
-                    }
-                    entry.path().split('\\').slice(0, -1).forEach(folder => {
-                        hook = hook.find(child => child.name === folder).children;
-                    })
-                    hook.push(node);
-                })
-
-                walker.on('end', function () {
-                    Promise.all(promises).then(() => {
-                        res.status(200).send(filesTree)
-                    }).catch(error => {
-                        console.log(error)
-                        res.status(500).send();
-                    })
-                })
-
-                walker.start();
-            })
+        history.on("commit", function(commit) {
+          list.push({
+            date: commit.date(),
+            author: commit.committer().name(),
+            message: commit.message(),
+            sha: commit.sha()
+          });
         });
-    }).catch(function (err) {
-        console.log(err);
-        res.status(404).send();
-    })
-})
 
-router.get('/repos/:owner/:repoName/commits/:sha/files/:path', function (req, res) {
-    getGitRepo(req.params).then(function (repo) {
-        return repo.getCommit(req.params.sha).then(function (commit) {
-            return commit.getEntry(req.params.path.replace(/\\/g, '/')).then(function (entry) {
-                if (entry && entry.isBlob()) {
-                    return entry.getBlob().then(function (blob) {
-                        res.status(200).send({ content: blob.content().toString() });
-                    })
-                } else {
-                    res.status(404).send();
-                }
-            })
+        history.on("end", function() {
+          res.status(200).json(list);
         });
-    }).catch(function (err) {
-        console.log(err);
-        res.status(404).send();
-    })
-})
 
-var getGitRepo = function (params) {
-    var { owner, repoName } = params;
-    var pathToRepo = path.join(__dirname, `repos/${owner}_${repoName}`);
-    var cloneOptions = {
-        fetchOpts: {
-            callbacks: {
-                transferProgress: function (log) {
-                    //console.log(`transfer : ${log.receivedObjects()} / ${log.totalObjects()} (${log.indexedObjects()})`);
-
-                }
-            }
-        }
-    }
-    return new Promise(function (resolve, reject) {
-        if (!owner || !repoName) {
-            reject('wrong parameters');
-            return;
-        }
-
-        Git.Repository.open(pathToRepo).then(resolve).catch(function (err) {
-            console.log('error while opening', err);
-            Git.Clone(`https://github.com/${owner}/${repoName}`, pathToRepo, cloneOptions)
-                .then(resolve)
-                .catch(function (err) {
-                    console.log('error while downloading', err);
-                    reject(err);
-                });
+        history.on("error", function(err) {
+          console.log(err);
+          res.status(500).send();
         });
+
+        history.start();
+      });
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(404).send();
     });
-}
+});
+
+router.get("/repos/:owner/:repoName/commits/:sha/files", async (req, res) => {
+  try {
+    const repo = await getGitRepo(req.params);
+    const commit = await repo.getCommit(req.params.sha);
+    const tree = await commit.getTree();
+    const filesTree = { name: req.params.repoName, children: [] };
+    const promises = [];
+    const walker = tree.walk(false);
+
+    walker.on("end", async trees => {
+      trees.forEach(async entry => {
+        let node = {name: entry.name()};
+        let hook = filesTree.children;
+        if (entry.isFile()) {
+          node.size = 0;
+          if (entry.isBlob()) {
+            const blobPromise = entry.getBlob();
+            promises.push(blobPromise);
+            const blob = await blobPromise;
+            node.size = blob
+              .content()
+              .toString()
+              .split("\n").length;
+          }
+        } else {
+          node.children = [];
+        }
+        console.log(entry.path())
+        entry
+          .path()
+          .split("/")
+          .slice(0, -1)
+          .forEach(folder => {
+            hook = hook.find(child => child.name === folder).children;
+          });
+        hook.push(node);
+      });
+      await Promise.all(promises);
+      console.log(filesTree);
+      res.status(200).send(filesTree);
+    });
+    walker.start();
+  } catch (err) {
+    console.log(err);
+    res.status(404).send();
+  }
+});
+
+var getGitRepo = async params => {
+  const { owner, repoName } = params;
+  const pathToRepo = path.join(__dirname, `repos/${owner}_${repoName}`);
+  if (!owner || !repoName) {
+    throw new Error("wrong parameters");
+  }
+
+  try {
+    return await Git.Repository.open(pathToRepo);
+  } catch (err) {
+    console.log("error while opening", err);
+    try {
+      return await Git.Clone(
+        `https://github.com/${owner}/${repoName}`,
+        pathToRepo
+      );
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+};
 
 module.exports = router;
--- src/scenes/commit/components/graph/index.js
+++ src/scenes/commit/components/graph/index.js
@@ -1,157 +1,262 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 import GitHubColors from "github-colors";
+import "./styles.css";
 
 export default class Graph extends Component {
+  componentDidMount() {
+    this.gitHubColors = GitHubColors.init(true);
+  }
+
+  //TODO clear graph when commit changes
   displayGraph(commit) {
-    let svg = d3.select("svg"),
-      margin = 20,
-      width = +svg.style("width").slice(0, -2),
-      height = +svg.style("height").slice(0, -2),
-      diameter = width > height ? height : width,
-      toDelete = svg.select("g");
-
-    let g = svg
-      .append("g")
-      .attr(
-        "transform",
-        "translate(" +
-          ((width - diameter) / 2 + diameter / 2) +
-          "," +
-          ((height - diameter) / 2 + diameter / 2) +
-          ")"
-      );
-
-    let color = d3
-      .scaleLinear()
-      .domain([-1, 5])
-      .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-      .interpolate(d3.interpolateHcl);
-
-    let pack = d3
-      .pack()
-      .size([diameter - margin, diameter - margin])
-      .padding(2);
-
-    let root = d3
-      .hierarchy(commit)
-      .sum(function(d) {
-        return d.size;
-      })
-      .sort(function(a, b) {
-        return b.name - a.name;
-      });
-
-    let focus = root,
-      nodes = pack(root).descendants(),
-      view;
-
-    let circle = g
-      .selectAll("circle")
-      .data(nodes)
-      .enter()
-      .append("circle")
-      .attr("class", function(d) {
-        return d.parent
-          ? d.children ? "node" : "node node--leaf"
-          : "node node--root";
-      })
-      .style("fill", function(d) {
-        return d.children ? color(d.depth) : calcColor(d.data.name);
-      })
-      .on("click", function(d) {
-        if (focus !== d) {
-          zoom(d);
-          d3.event.stopPropagation();
+    const getColor = name => {
+      if (name) {
+        let ext = name.split(".")[name.split(".").length - 1];
+        if (this.gitHubColors[ext] && this.gitHubColors[ext].color) {
+          return this.gitHubColors[ext].color;
         }
-      });
-
-    g
-      .selectAll("text")
-      .data(nodes)
-      .enter()
-      .append("text")
-      .attr("class", "label")
-      .style("fill-opacity", function(d) {
-        return d.parent === root ? 1 : 0;
-      })
-      .style("display", function(d) {
-        return d.parent === root ? "inline" : "none";
-      })
-      .text(function(d) {
-        return d.data.name;
-      });
-
-    let node = g.selectAll("circle,text");
-
-    svg.on("click", function() {
-      zoom(root);
-    });
-
-    zoomTo([root.x, root.y, root.r * 2 + margin]);
-
-    toDelete.remove();
-
-    function zoomTo(v) {
-      let k = diameter / v[2];
-      view = v;
-      node.attr("transform", function(d) {
-        return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")";
-      });
-      circle.attr("r", function(d) {
-        return d.r * k;
-      });
-    }
-    function zoom(d) {
-      if (d.children) {
-        focus = d;
-
-        let transition = d3
-          .transition()
-          .duration(d3.event.altKey ? 7500 : 750)
-          .tween("zoom", function(d) {
-            let i = d3.interpolateZoom(view, [
-              focus.x,
-              focus.y,
-              focus.r * 2 + margin
-            ]);
-            return function(t) {
-              zoomTo(i(t));
-            };
-          });
-
-        transition
-          .selectAll("text")
-          .filter(function(d) {
-            return d.parent === focus || this.style.display === "inline";
-          })
-          .style("fill-opacity", function(d) {
-            return d.parent === focus ? 1 : 0;
-          })
-          .on("start", function(d) {
-            if (d.parent === focus) this.style.display = "inline";
-          })
-          .on("end", function(d) {
-            if (d.parent !== focus) this.style.display = "none";
-          });
       }
+      return "#ccc";
+    };
+
+    // Dimensions of sunburst.
+    var width = 750;
+    var height = 600;
+    var radius = Math.min(width, height) / 2;
+
+    // Breadcrumb dimensions: width, height, spacing, width of tip/tail.
+    var b = {
+      w: 120,
+      h: 30,
+      s: 3,
+      t: 10
+    };
+
+    // Total size of all segments; we set this later, after loading the data.
+    var totalSize = 0;
+
+    var vis = d3
+      .select("#chart svg")
+      .attr("width", width)
+      .attr("height", height)
+      .append("svg:g")
+      .attr("id", "container")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var partition = d3.partition().size([2 * Math.PI, radius * radius]);
+
+    var arc = d3
+      .arc()
+      .startAngle(function(d) {
+        return d.x0;
+      })
+      .endAngle(function(d) {
+        return d.x1;
+      })
+      .innerRadius(function(d) {
+        return Math.sqrt(d.y0);
+      })
+      .outerRadius(function(d) {
+        return Math.sqrt(d.y1);
+      });
+
+    createVisualization(commit);
+
+    // Main function to draw and set up the visualization, once we have the data.
+    function createVisualization(json) {
+      // Basic setup of page elements.
+      initializeBreadcrumbTrail();
+
+      // Bounding circle underneath the sunburst, to make it easier to detect
+      // when the mouse leaves the parent g.
+      vis
+        .append("svg:circle")
+        .attr("r", radius)
+        .style("opacity", 0);
+
+      // Turn the data into a d3 hierarchy and calculate the sums.
+      var root = d3
+        .hierarchy(json)
+        .sum(function(d) {
+          return d.size;
+        })
+        .sort(function(a, b) {
+          return b.value - a.value;
+        });
+
+      // For efficiency, filter nodes to keep only those large enough to see.
+      var nodes = partition(root)
+        .descendants()
+        .filter(function(d) {
+          return d.x1 - d.x0 > 0.005; // 0.005 radians = 0.29 degrees
+        });
+
+      var path = vis
+        .data([json])
+        .selectAll("path")
+        .data(nodes)
+        .enter()
+        .append("svg:path")
+        .attr("display", function(d) {
+          return d.depth ? null : "none";
+        })
+        .attr("d", arc)
+        .attr("fill-rule", "evenodd")
+        .style("fill", function(d) {
+          return getColor(d.data.name);
+        })
+        .style("opacity", 1)
+        .on("mouseover", mouseover);
+
+      // Add the mouseleave handler to the bounding circle.
+      d3.select("#container").on("mouseleave", mouseleave);
+
+      // Get total size of the tree = value of root node from partition.
+      totalSize = path.datum().value;
+    }
+
+    // Fade all but the current sequence, and show it in the breadcrumb trail.
+    function mouseover(d) {
+      var size = d.value;
+      let centerText = `${size} / ${totalSize}`;
+      d3.select("#percentage").text(centerText);
+      d3.select("#explanation").style("visibility", "");
+      var sequenceArray = d.ancestors().reverse();
+      sequenceArray.shift(); // remove root node from the array
+      updateBreadcrumbs(sequenceArray, size);
+
+      // Fade all the segments.
+      d3.selectAll("path").style("opacity", 0.3);
+
+      // Then highlight only those that are an ancestor of the current segment.
+      vis
+        .selectAll("path")
+        .filter(function(node) {
+          return sequenceArray.indexOf(node) >= 0;
+        })
+        .style("opacity", 1);
+    }
+
+    // Restore everything to full opacity when moving off the visualization.
+    function mouseleave(d) {
+      // Hide the breadcrumb trail
+      d3.select("#trail").style("visibility", "hidden");
+
+      // Deactivate all segments during transition.
+      d3.selectAll("path").on("mouseover", null);
+
+      // Transition each segment to full opacity and then reactivate it.
+      d3
+        .selectAll("path")
+        .transition()
+        .duration(1000)
+        .style("opacity", 1)
+        .on("end", function() {
+          d3.select(this).on("mouseover", mouseover);
+        });
+
+      d3.select("#explanation").style("visibility", "hidden");
+    }
+
+    function initializeBreadcrumbTrail() {
+      // Add the svg area.
+      var trail = d3
+        .select("#sequence svg")
+        .attr("width", width)
+        .attr("height", 50)
+        .attr("id", "trail");
+      // Add the label at the end, for the percentage.
+      trail
+        .append("svg:text")
+        .attr("id", "endlabel")
+        .style("fill", "#000");
+    }
+
+    // Generate a string that describes the points of a breadcrumb polygon.
+    function breadcrumbPoints(d, i) {
+      var points = [];
+      points.push("0,0");
+      points.push(b.w + ",0");
+      points.push(b.w + b.t + "," + b.h / 2);
+      points.push(b.w + "," + b.h);
+      points.push("0," + b.h);
+      if (i > 0) {
+        // Leftmost breadcrumb; don't include 6th vertex.
+        points.push(b.t + "," + b.h / 2);
+      }
+      return points.join(" ");
+    }
+
+    // Update the breadcrumb trail to show the current sequence and percentage.
+    function updateBreadcrumbs(nodeArray, hoveredElInfo) {
+      // Data join; key function combines name and depth (= position in sequence).
+      var trail = d3
+        .select("#trail")
+        .selectAll("g")
+        .data(nodeArray, function(d) {
+          return d.data.name + d.depth;
+        });
+
+      // Remove exiting nodes.
+      trail.exit().remove();
+
+      // Add breadcrumb and label for entering nodes.
+      var entering = trail.enter().append("svg:g");
+
+      entering
+        .append("svg:polygon")
+        .attr("points", breadcrumbPoints)
+        .style("fill", function(d) {
+          return getColor(d.data.name);
+        });
+
+      entering
+        .append("svg:text")
+        .attr("x", (b.w + b.t) / 2)
+        .attr("y", b.h / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+          return d.data.name;
+        });
+
+      // Merge enter and update selections; set position for all nodes.
+      entering.merge(trail).attr("transform", function(d, i) {
+        return "translate(" + i * (b.w + b.s) + ", 0)";
+      });
+
+      // Now move and update the percentage at the end.
+      d3
+        .select("#trail")
+        .select("#endlabel")
+        .attr("x", (nodeArray.length + 0.5) * (b.w + b.s))
+        .attr("y", b.h / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(hoveredElInfo);
+
+      // Make the breadcrumb trail visible, if it's hidden.
+      d3.select("#trail").style("visibility", "");
     }
   }
 
   render() {
-    if(this.props.commit) this.displayGraph(this.props.commit);
-    return <svg width="100%" height="100%" />;
+    if (this.props.commit) this.displayGraph(this.props.commit);
+    return (
+      <div id="main">
+        <div id="sequence">
+          <svg />
+        </div>
+        <div id="chart">
+          <div id="explanation" style={{ visibility: "hidden" }}>
+            <span id="percentage" />
+            <br />
+            Charactères
+          </div>
+          <svg />
+        </div>
+      </div>
+    );
   }
-}
-
-
-//HELPER FUNCTION ////////////////////////////
-function calcColor(name) {
-  if (name) {
-    let ext = name.split(".")[name.split(".").length - 1];
-    if (GitHubColors[ext] && GitHubColors[ext].color) {
-      return GitHubColors[ext].color;
-    }
-  }
-  return "#ccc";
 }
